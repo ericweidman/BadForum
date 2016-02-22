f508af34600af3f972d91c9d9029828d14c22929
ericweidman <ericweidman@gmail.com>
2016-02-22 13:01:18 -0500

    Initial import. Added Spark + Mustache

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,9 +1,38 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
+    static User user;
+
     public static void main(String[] args) {
-	// write your code here
+        Spark.init();
+
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    if(user == null) {
+                        return new ModelAndView(m, "login.html");
+                    }else {
+                        m.put("name", user.name);
+                        return new ModelAndView(m, "index.html");
+                    }
+                    }),
+                new MustacheTemplateEngine()
+        );
+
+
+
+
+
+
     }
 }
 
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -4,4 +4,12 @@ package com.theironyard;
  * Created by ericweidman on 2/22/16.
  */
 public class User {
+    String name;
+    String userText;
+    String userPassword;
+
+    public User(String name, String userText) {
+        this.name = name;
+        this.userText = userText;
+    }
 }
