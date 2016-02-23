2f6a5de6f7c45f84923ea1f5bac949328b985fbb
ericweidman <ericweidman@gmail.com>
2016-02-22 19:05:56 -0500

    Made pretty-ish.

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -57,26 +57,3 @@ public class Main {
 
     }
 }
-
-
-
-
-
-
-//        Create resources/templates/index.html which looks like in the first screenshot below.
-
-//        Create a GET route for "/messages" and a POST route for "/create-user" and "/create-text".
-
-//        When the user hits submit in "index.html", it should post the name to "/create-user"
-//        which saves it in a user object and redirects to "/messages".
-
-//        Create resources/templates/messages.html which looks like the second screenshot below.
-//        It must display the name given by the user, and use Mustache templating
-//        to list the messages written by the user.
-
-//        When the user hits submit in "messages.html", it should post the text to "/create-text"
-//        which saves it in an ArrayList<com.theironyard.Message> and redirects to "/messages" (i.e. refreshes the page).
-//        like usual; if it does exist, then check the password and return an error if it is wrong.
-
-//        Optional: In "index.html", add a password field. If the user doesn't exist, have it behave
-//        Optional: Add styling with CSS.
